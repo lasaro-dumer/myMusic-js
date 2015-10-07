4c5a239c113fac8def07d5dcf10d05cf6634c83e
Lásaro <ladumer@hotmail.com>
2015-10-07 13:53:58 -0300

    Grafical Interface
    +bootstrap
    +jQuery
    +Jade views
    +favicon

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var moment = require('moment');
 
-var server_port = process.env.PORT || 5000
+var server_port = process.env.PORT || 1337
 //var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var app = express();
 
@@ -21,16 +21,16 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-//app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 exports.index = function(req, res){
-    res.send('oi')
+    res.render('index')
 }
 
 var http = require('http'),
